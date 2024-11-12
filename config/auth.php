fb9dd0885ce59049339436c89034d730089d0a52
Muhammad Abdullah <m.abdullah.fuf@gmail.com>
2024-11-12 17:35:39 +0500

    just add teacher model with migration file and confing/auth guard

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,10 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'teacher' => [
+            'driver' => 'session',
+            'provider' => 'teachers',
+        ],
     ],
 
     /*
@@ -64,7 +68,10 @@ return [
             'driver' => 'eloquent',
             'model' => env('AUTH_MODEL', App\Models\User::class),
         ],
-
+        'teachers' => [
+            'driver' => 'eloquent',
+            'model' => env('AUTH_MODEL', App\Models\Teacher::class),
+        ],
         // 'users' => [
         //     'driver' => 'database',
         //     'table' => 'users',
